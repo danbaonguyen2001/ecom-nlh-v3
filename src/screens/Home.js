07c9094e0ec264c4fa95007523e947a913f77b1b
unknown <danbaonguyen2001@gmail.com>
2023-03-12 22:12:00 +0700

    Home Page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
 
 import Banner from "../components/home/Banner";
+import Deal from "../components/home/Deal";
+import Technology from "../components/home/Technology";
+import Trademark from "../components/home/Trademark";
+import { TopProduct } from "../components/product/TopProduct";
 
 const Home = () => {
   return (
-    <div>
+    <div className="m-2 lg:m-0">
       <Banner />
-      {/* <ProductList /> */}
+      <Technology />
+      <Trademark />
+      <Deal />
+      <TopProduct />
     </div>
   );
 };
