b544717ec33407c52d1717d076004d59e2c10222
unknown <danbaonguyen2001@gmail.com>
2023-03-21 21:54:07 +0700

    9:53 Momo

--- src/actions/GHNActions.js
+++ src/actions/GHNActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 
 import {
   GET_PROVINCE_LIST_REQUEST,
@@ -13,28 +13,28 @@ import {
   GET_SHIPPING_FEE_REQUEST,
   GET_SHIPPING_FEE_SUCCESS,
   GET_SHIPPING_FEE_FAIL,
-} from '../constants/GHNConstants'
+} from "../constants/GHNConstants";
 import {
   Token_API_GHN,
   GHN,
   GHN_CALCULATE_FEE,
   SHOP_ID,
   district_id,
-} from '../apis/Api'
+} from "../apis/Api";
 export const getProvinceList = () => async (dispatch) => {
   try {
-    dispatch({ type: GET_PROVINCE_LIST_REQUEST })
+    dispatch({ type: GET_PROVINCE_LIST_REQUEST });
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Token: `${Token_API_GHN}`,
       },
-    }
-    const { data } = await axios.get(`${GHN}/province`, config)
+    };
+    const { data } = await axios.get(`${GHN}/province`, config);
     dispatch({
       type: GET_PROVINCE_LIST_SUCCESS,
       payload: data,
-    })
+    });
   } catch (error) {
     dispatch({
       type: GET_PROVINCE_LIST_FAIL,
@@ -42,32 +42,32 @@ export const getProvinceList = () => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    })
+    });
   }
-}
+};
 export const getDistrictList = (provinceId) => async (dispatch) => {
   try {
-    dispatch({ type: GET_DISTRICT_LIST_REQUEST })
+    dispatch({ type: GET_DISTRICT_LIST_REQUEST });
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Token: `${Token_API_GHN}`,
       },
-    }
+    };
     const { data } = await axios.get(`${GHN}/district`, {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Token: `${Token_API_GHN}`,
       },
       params: {
         province_id: provinceId,
       },
-    })
+    });
 
     dispatch({
       type: GET_DISTRICT_LIST_SUCCESS,
       payload: data,
-    })
+    });
   } catch (error) {
     dispatch({
       type: GET_DISTRICT_LIST_FAIL,
@@ -75,26 +75,26 @@ export const getDistrictList = (provinceId) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    })
+    });
   }
-}
+};
 export const getWardList = (districtId) => async (dispatch) => {
   try {
-    dispatch({ type: GET_WARD_LIST_REQUEST })
+    dispatch({ type: GET_WARD_LIST_REQUEST });
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Token: `${Token_API_GHN}`,
       },
-    }
+    };
     const { data } = await axios.get(
       `${GHN}/ward?district_id=${districtId}`,
       config
-    )
+    );
     dispatch({
       type: GET_WARD_LIST_SUCCESS,
       payload: data,
-    })
+    });
   } catch (error) {
     dispatch({
       type: GET_WARD_LIST_FAIL,
@@ -102,20 +102,20 @@ export const getWardList = (districtId) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    })
+    });
   }
-}
+};
 export const getShippingFe = (ward) => async (dispatch) => {
   try {
     dispatch({
       type: GET_SHIPPING_FEE_REQUEST,
-    })
+    });
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Token: `${Token_API_GHN}`,
       },
-    }
+    };
     const { data } = await axios.post(
       `${GHN_CALCULATE_FEE}`,
       {
@@ -123,16 +123,16 @@ export const getShippingFe = (ward) => async (dispatch) => {
         service_type_id: null, // Loại dịch vụ vận chuyển
         shop_id: SHOP_ID, //Quản lý thông tin cửa hàng/người bán
         from_district_id: district_id, // Mã quận/huyện nơi gửi hàng
-        to_district_id: ward.DistrictID, // Mã quận/huyện nơi nhận hàng
-        to_ward_code: ward.WardCode, // Mã Mã phường nhận bưu kiện.
+        to_district_id: ward?.DistrictID, // Mã quận/huyện nơi nhận hàng
+        to_ward_code: ward?.WardCode, // Mã Mã phường nhận bưu kiện.
         weight: 500, // Khối lượng gói hàng (gram)
         length: 20, // Chiều dài (cm)
         width: 20, // Chiều rộng (cm)
         height: 10, // Chiều cao (cm)
       },
       config
-    )
-    dispatch({ type: GET_SHIPPING_FEE_SUCCESS, payload: data })
+    );
+    dispatch({ type: GET_SHIPPING_FEE_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
       type: GET_SHIPPING_FEE_FAIL,
@@ -140,6 +140,6 @@ export const getShippingFe = (ward) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    })
+    });
   }
-}
+};
